2c16c7d0a0c726456402897139a4b084c9c7274b
vadimpavlovskiy <vadimpavlovskiy@icloud.com>
2024-01-01 18:07:42 -0700

    Created a base canvas to work with graphic

--- app/components/imageComponent/imageComponent.tsx
+++ app/components/imageComponent/imageComponent.tsx
@@ -1,11 +1,29 @@
 import { IImageComponent } from "@/app/@types/componetnsTypes/imageComponents"
 import Image from "next/image"
+import { useEffect, useRef } from "react"
 
 
 export const ImageComponet = ({src}:IImageComponent) => { // Image Component shows image
-return (
+const baseCanvas = useRef<HTMLCanvasElement>(null);
+
+useEffect(() => {
+    if(baseCanvas.current) {
+        const canvasContext = baseCanvas.current.getContext('2d');
+        const canvasImage = new window.Image();
+
+        canvasImage.src = src;
+
+        canvasImage.onload = () => {
+            if(baseCanvas) {
+                canvasContext?.drawImage(canvasImage, 0, 0, 500, 500);
+            }
+          };
+    }
+}, [])
+    return (
     <>
-        <img style={{width: '50%', height: '50%'}} alt="Image Selected" src={src} /> 
+        {/* <img style={{width: '50%', height: '50%'}} alt="Image Selected" src={src} /> */}
+        <canvas ref={baseCanvas} width="600px" height={'600px'} /> 
     </>
 )
 }
